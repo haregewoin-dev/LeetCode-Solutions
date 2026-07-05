0c440c38a315f609d069b896e579e207aeae2062
haregewoin-dev <haregewoin624@gmail.com>
2026-07-05 12:58:21 +0300

    Time: 13 ms (56.45%), Space: 56.6 MB (46.45%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numberOfSubstrings = function(s) {
+       const count = [0, 0, 0]; 
+    let left = 0;
+    let ans = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        count[s.charCodeAt(right) - 97]++;
+
+        while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+            ans += s.length - right;
+            count[s.charCodeAt(left) - 97]--;
+            left++;
+        }
+    }
+
+    return ans;
+
+};
